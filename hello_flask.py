44c117ac25c6cc12c04d4ec6abc92634e4aa280c
Vera Boukhonine <veraboukhonine123@gmail.com>
2024-07-07 15:28:21 -0700

    comments added

--- hello_flask.py
+++ hello_flask.py
@@ -32,12 +32,14 @@ def vera():
 @app.route("/mytemplate")
 def t_test():
     return render_template("index.html")
-
+# Task 5
+# Link to git repo: https://github.com/vboukhonine/lab_flask_1
 
 """
 Lab Completion Report: 
 I wasn't able to complete task 2 I didn't give myself enough time to ask anyone else in the class what their favorite acronyms 
 are and procrastinated too much due to fatigue from the heat wave.
+
 References: 
 https://www.tutorialspoint.com/How-to-set-Text-alignment-in-HTML
 """
